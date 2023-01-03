fd08d8c5d6045654ba4c2ad5c68129114f9adebe
fernando_alfonso_zhunio <fzhunio91@hotmail.com>
2023-01-03 18:29:32 -0500

    latest changes

--- node/utils/validationSaveMasterdata.ts
+++ node/utils/validationSaveMasterdata.ts
@@ -8,6 +8,7 @@ const keysOfStore = [
   'schedules',
 ]
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function validationSaveMasterdata(data: any) {
   new Validator(data)
     .isObject()
